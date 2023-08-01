c735c1a35708a74936aa95dd593715bf64218342
Yann Vigara <yvigara@gmail.com>
2023-08-01 21:51:44 +1000

    Refactor to bring project closer to other prometheus exporters

--- main.go
+++ main.go
@@ -4,41 +4,50 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
-	"gopkg.in/alecthomas/kingpin.v2"
+	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
 )
+
+func init() {
+	prometheus.MustRegister(version.NewCollector("freeswitch_exporter"))
+}
 
 func main() {
 	var (
-		listenAddress = kingpin.Flag(
-			"web.listen-address",
-			"Address to listen on for web interface and telemetry.").Short('l').Default(":9282").String()
 		metricsPath = kingpin.Flag(
 			"web.telemetry-path",
-			"Path under which to expose metrics.").Default("/metrics").String()
+			"Path under which to expose metrics.",
+		).Default("/metrics").String()
 		scrapeURI = kingpin.Flag(
 			"freeswitch.scrape-uri",
-			`URI on which to scrape freeswitch. E.g. "tcp://localhost:8021"`).Short('u').Default("tcp://localhost:8021").String()
+			`URI on which to scrape freeswitch. E.g. "tcp://localhost:8021"`,
+		).Short('u').Default("tcp://localhost:8021").String()
 		timeout = kingpin.Flag(
 			"freeswitch.timeout",
-			"Timeout for trying to get stats from freeswitch.").Short('t').Default("5s").Duration()
+			"Timeout for trying to get stats from freeswitch.",
+		).Short('t').Default("5s").Duration()
 		password = kingpin.Flag(
 			"freeswitch.password",
-			"Password for freeswitch event socket.").Short('P').Default("ClueCon").String()
-		configFile = kingpin.Flag(
-			"web.config",
-			"[EXPERIMENTAL] Path to config yaml file that can enable TLS or authentication.",
-		).Default("").String()
-		rtpEnable = kingpin.Flag("rtp.enable", "enable rtp info, default: fasle").Default("false").Bool()
+			"Password for freeswitch event socket.",
+		).Short('P').Default("ClueCon").String()
+		toolkitFlags = webflag.AddFlags(kingpin.CommandLine, ":9282")
+		rtpEnable    = kingpin.Flag("rtp.enable", "enable rtp info, default: fasle").Default("false").Bool()
 	)
 	promlogConfig := &promlog.Config{}
-	kingpin.Version("freeswitch_exporter")
-	logger := promlog.New(promlogConfig)
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("freeswitch_exporter"))
 	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
+
+	level.Info(logger).Log("msg", "Starting freeswitch_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
 
 	c, err := NewCollector(*scrapeURI, *timeout, *password, *rtpEnable)
 
@@ -49,19 +58,27 @@ func main() {
 	prometheus.MustRegister(c)
 
 	http.Handle(*metricsPath, promhttp.Handler())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-			<head><title>FreeSWITCH Exporter</title></head>
-			<body>
-			<h1>FreeSWITCH Exporter</h1>
-			<p><a href="` + *metricsPath + `">Metrics</a></p>
-			</body>
-			</html>`))
-	})
-
-	level.Info(logger).Log("msg", "Listening on", "address", *listenAddress)
-	server := &http.Server{Addr: *listenAddress}
-	if err := web.ListenAndServe(server, *configFile, logger); err != nil {
+	if *metricsPath != "/" && *metricsPath != "" {
+		landingConfig := web.LandingConfig{
+			Name:        "FreeSWITCH Exporter",
+			Description: "Prometheus Exporter for FreeSWITCH servers",
+			Version:     version.Info(),
+			Links: []web.LandingLinks{
+				{
+					Address: *metricsPath,
+					Text:    "Metrics",
+				},
+			},
+		}
+		landingPage, err := web.NewLandingPage(landingConfig)
+		if err != nil {
+			level.Error(logger).Log("err", err)
+			os.Exit(1)
+		}
+		http.Handle("/", landingPage)
+	}
+	server := &http.Server{}
+	if err := web.ListenAndServe(server, toolkitFlags, logger); err != nil {
 		level.Info(logger).Log("err", err)
 		os.Exit(1)
 	}
